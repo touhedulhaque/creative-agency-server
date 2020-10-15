47fe8c06ab89a842383d8c3e2c9192535c4bc76b
touhedulhaque <razmbamkt@gmail.com>
2020-10-15 17:44:31 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,99 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const MongoClient = require('mongodb').MongoClient;
+require('dotenv').config();
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ujfln.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const port = 5000;
+
+app.get('/', (req, res) => {
+    res.send('hello from DB it is working perfectly')
+})
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+    const orderCollection = client.db("creativeAgency").collection("orders");
+    const reviewCollection = client.db("creativeAgency").collection("reviews");
+    const serviceCollection = client.db("creativeAgency").collection("services");
+    const adminCollection = client.db("creativeAgency").collection("admins");
+
+    app.post('/addOrder', (req, res) => {
+        const order = req.body;
+        orderCollection.insertOne(order)
+            .then(result => {
+                res.send(result.insertedCount > 0)
+            })
+    })
+
+    app.get('/orders', (req, res) => {
+        orderCollection.find({})
+            .toArray((err, documents) => {
+                res.send(documents)
+            })
+    })
+
+    
+    
+    app.get('/servicelistByEmail', (req, res) => {
+        email = req.query.email
+        orderCollection.find({ email })
+            .toArray((err, documents) => {
+                res.send(documents);
+            })
+    })
+
+    app.post('/addReview', (req, res) => {
+        const review = req.body;
+        reviewCollection.insertOne(review)
+            .then(result => {
+                res.send(result.insertedCount > 0)
+            })
+    })
+
+    app.get('/reviews', (req, res) => {
+        reviewCollection.find({})
+            .toArray((err, documents) => {
+                res.send(documents)
+            })
+    })
+
+    app.post('/addService', (req, res) => {
+        const service = req.body;
+        serviceCollection.insertOne(service)
+            .then(result => {
+                res.send(result.insertedCount > 0)
+            })
+    })
+
+    app.get('/services', (req, res) => {
+        serviceCollection.find({})
+            .toArray((err, documents) => {
+                res.send(documents)
+            })
+    })
+
+    app.post('/addAdmin', (req, res) => {
+        const admin = req.body;
+        adminCollection.insertOne(admin)
+            .then(result => {
+                res.send(result.insertedCount > 0)
+            })
+    })
+
+
+});
+
+
+
+
+
+
+app.listen(process.env.PORT || port)
